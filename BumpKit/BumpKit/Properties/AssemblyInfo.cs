2f65fb43060387c31bbbedecf4998b4d192566c2
datadink <markerdink@gmail.com>
2012-08-05 08:07:53 -0700

    Created Nuget
    Removed unused references and created a nuget package

--- BumpKit/BumpKit/Properties/AssemblyInfo.cs
+++ BumpKit/BumpKit/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BumpKit")]
-[assembly: AssemblyDescription("A free, open-source, library extending System.Drawing.")]
+[assembly: AssemblyDescription("https://github.com/DataDink/Bumpkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mark Nelson")]
 [assembly: AssemblyProduct("BumpKit")]
